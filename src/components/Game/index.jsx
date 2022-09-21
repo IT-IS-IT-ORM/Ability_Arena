1cdcf01ce8e71eb71f44e6544b451d4ef39ef418
YernarT <toktaryernar@gmail.com>
2022-09-21 16:16:01 +0600

    wip: :construction: Game

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,5 +1,65 @@
-import { GameStyled } from './styled';
+import { useState } from 'react';
+
+import { getRandomContent } from '../../utils';
+
+import { SiAcclaim } from 'react-icons/si';
+import { TiArrowBack } from 'react-icons/ti';
+import { ImShuffle } from 'react-icons/im';
+
+import Card from './Card';
+import Slot from './Slot';
+import ActionBtn from './ActionBtn';
+import {
+	GameStyled,
+	CardContainer,
+	DrawnCards,
+	SlotContainer,
+	Toolbar,
+} from './styled';
+
+// 初始化卡槽信息
+const initSlots = [];
+for (let i = 1; i <= 7; i++) {
+	initSlots.push({
+		id: i,
+		isEmpty: true,
+		card: null,
+	});
+}
 
 export default function Game() {
-	return <GameStyled>Game</GameStyled>;
+	const [title, setTitle] = useState('First Round');
+	const [slots, setSlots] = useState(initSlots);
+
+	return (
+		<GameStyled>
+			{/* 关卡信息 */}
+			<p className="title">{title}</p>
+
+			{/* 所有卡牌 */}
+			<CardContainer>
+				<Card />
+			</CardContainer>
+
+			{/* 抽出的卡牌 */}
+			<DrawnCards></DrawnCards>
+
+			{/* 卡槽 */}
+			<SlotContainer slots={slots}>
+				{slots.map(slot => (
+					<Slot key={slot.id} />
+				))}
+			</SlotContainer>
+
+			{/* 道具栏 */}
+			<Toolbar>
+				{/* 移出 */}
+				<ActionBtn icon={<SiAcclaim />} />
+				{/* 撤回 */}
+				<ActionBtn icon={<TiArrowBack />} />
+				{/* 打乱 */}
+				<ActionBtn icon={<ImShuffle />} />
+			</Toolbar>
+		</GameStyled>
+	);
 }
--- src/components/Game/styled.js
+++ src/components/Game/styled.js
@@ -1,3 +1,56 @@
 import styled from 'styled-components';
 
-export const GameStyled = styled.main``;
+export const GameStyled = styled.main`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	.title {
+		font-size: 20px;
+		color: #ffff;
+	}
+`;
+
+export const CardContainer = styled.div`
+	width: 100%;
+	/* height: 340px; */
+    flex-grow: 1;
+
+	margin-top: 12px;
+	margin-bottom: 8px;
+	padding: 20px;
+	border: 1px solid #fff;
+
+	position: relative;
+`;
+
+export const DrawnCards = styled.div`
+	width: 100%;
+	height: 32px;
+	border: 1px solid #fff;
+
+	margin-top: auto;
+	margin-bottom: 8px;
+`;
+
+export const SlotContainer = styled.div`
+	width: 100%;
+	height: 32px;
+
+	border: 1px solid #fff;
+	border-top: none;
+    margin-bottom: 8px;
+
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+`;
+
+export const Toolbar = styled.div`
+	width: 100%;
+	height: 44px;
+
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+`;
