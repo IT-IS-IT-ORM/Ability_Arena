94b80fa41f2c1592b5806ed8191b75a6585a854d
YernarT <toktaryernar@gmail.com>
2022-09-21 16:06:38 +0600

    init: :rainbow: Slot

---
+++ src/components/Game/Slot/index.jsx
@@ -0,0 +1,5 @@
+import { SlotStyled } from './styled';
+
+export default function Slot() {
+	return <SlotStyled></SlotStyled>;
+}
---
+++ src/components/Game/Slot/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SlotStyled = styled.div`
+	width: 32px;
+	height: 32px;
+
+	background: #000000;
+	background: linear-gradient(135deg, #434343, #000000);
+
+	border: 1px solid #fff;
+`;
