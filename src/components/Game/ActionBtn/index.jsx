e5aff0d8c69a81a8be024d2e883e45b823b5d5db
YernarT <toktaryernar@gmail.com>
2022-09-21 16:15:50 +0600

    wip: :construction: ActionBtn

---
+++ src/components/Game/ActionBtn/index.jsx
@@ -0,0 +1,5 @@
+import { ActionBtnStyled } from './styled';
+
+export default function ActionBtn({ icon }) {
+	return <ActionBtnStyled>{icon}</ActionBtnStyled>;
+}
---
+++ src/components/Game/ActionBtn/styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ActionBtnStyled = styled.button`
+	${({ theme }) => theme.typography.button.apply};
+`;
