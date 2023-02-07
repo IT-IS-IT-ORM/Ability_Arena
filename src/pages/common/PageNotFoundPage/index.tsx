b3d51ada45e8ebe015c3486972464549658d8661
YernarT <toktaryernar@gmail.com>
2023-02-07 23:35:43 +0600

    init: :rainbow: 404 page

---
+++ src/pages/common/PageNotFoundPage/index.tsx
@@ -0,0 +1,11 @@
+// i18n
+import { useTranslation } from 'react-i18next';
+
+// 样式组件
+import { PageNotFoundPageStyled } from './style';
+
+export default function PageNotFoundPage() {
+	const { t } = useTranslation();
+
+	return <PageNotFoundPageStyled status={404} title={t('resourceNotFound')} />;
+}
---
+++ src/pages/common/PageNotFoundPage/style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+// 组件
+import { Result } from 'antd';
+
+export const PageNotFoundPageStyled = styled(Result)`
+	.ant-result-title {
+		color: #f0f0f0;
+	}
+`;
