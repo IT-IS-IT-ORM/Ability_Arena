55a6851dd296da23e1fcc5cfe79ade40ebf682f8
YernarT <toktaryernar@gmail.com>
2023-02-07 23:35:21 +0600

    init: :rainbow: initial avatars

---
+++ src/assets/image/index.ts
@@ -0,0 +1,10 @@
+// 用户头像
+export { default as Avatar_1 } from './avatar_1.jpg';
+export { default as Avatar_2 } from './avatar_2.jpg';
+export { default as Avatar_3 } from './avatar_3.jpg';
+export { default as Avatar_4 } from './avatar_4.jpg';
+export { default as Avatar_5 } from './avatar_5.jpg';
+export { default as Avatar_6 } from './avatar_6.jpg';
+export { default as Avatar_7 } from './avatar_7.jpg';
+export { default as Avatar_8 } from './avatar_8.jpg';
+export { default as Avatar_9 } from './avatar_9.jpg';
