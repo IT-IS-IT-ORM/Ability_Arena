c974cc8f5510bcf27b0903f36a42426ba4574416
YernarT <toktaryernar@gmail.com>
2023-02-10 16:35:09 +0600

    feat: :sparkles: 公共骨架屏

---
+++ src/components/skeleton/CommonLoadingSkeleton/index.tsx
@@ -0,0 +1,9 @@
+import { CommonLoadingSkeletonStyled } from './style';
+
+export default function CommonLoadingSkeleton() {
+	return (
+		<CommonLoadingSkeletonStyled>
+			<div className="circle"></div>
+		</CommonLoadingSkeletonStyled>
+	);
+}
