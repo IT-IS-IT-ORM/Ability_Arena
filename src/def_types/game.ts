264fbbb1ee69629375e0f063cf006d3d1ca87652
YernarT <toktaryernar@gmail.com>
2023-02-12 16:56:39 +0600

    feat: :hammer: cover is required

--- src/def_types/game.ts
+++ src/def_types/game.ts
@@ -2,7 +2,7 @@ export type gameStatus = 'Planning' | 'InProgress' | 'Beta' | 'Completed';
 
 export interface I_Game {
 	name: string;
-	cover?: string;
+	cover: string;
 	status: gameStatus;
 	hasBot: boolean;
 }
