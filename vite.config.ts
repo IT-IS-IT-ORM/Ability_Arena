53b7abc2b68319f888dd7a5b8d915a384c13b43b
YernarT <toktaryernar@gmail.com>
2023-04-02 23:39:24 +0600

    update: :hammer: postcss 配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 import path from 'path';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,16 @@ export default defineConfig({
 			'@': path.resolve(path.resolve(), './src'),
 		},
 	},
+
 	plugins: [react()],
+
+	css: {
+		postcss: {
+			plugins: [
+				autoprefixer({
+					overrideBrowserslist: 'last 2 versions',
+				}),
+			],
+		},
+	},
 });
