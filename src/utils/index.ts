94015e0163ad52e30aac5b8b8c9a8619a7f5a452
YernarT <toktaryernar@gmail.com>
2023-04-03 23:05:47 +0600

    update: :hammer: set font instead change lang

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,5 +6,5 @@ export { localStorage } from '@/utils/localStorage';
 export type { allowedRole } from '@/utils/role';
 export { role } from '@/utils/role';
 
-export { default as getHtmlLang } from './getHtmlLang';
 export { default as getAntdLocale } from './getAntdLocale';
+export { default as setFontByLocale } from './setFontByLocale';
