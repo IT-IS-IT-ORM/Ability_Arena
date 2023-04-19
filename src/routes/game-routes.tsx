5cb312f032ba6caa372161606de0becfc9f3041c
YernarT <toktaryernar@gmail.com>
2023-04-19 17:21:23 +0600

    feat: :sparkles: 新增游戏页

---
+++ src/routes/game-routes.tsx
@@ -0,0 +1,19 @@
+import { I_Route } from '@/def_types/route';
+
+import { lazy } from 'react';
+import { CommonLoadingSkeleton } from '@/components/skeleton';
+import { role } from '@/utils';
+
+const gameRoutes: I_Route[] = [
+	{
+		path: '/games/liarCard',
+		component: lazy(() => import('@/pages/game/LiarCard')),
+		fallback: <CommonLoadingSkeleton />,
+		auth: false,
+		role: role.all(),
+		title: 'Playground',
+		exact: true,
+	},
+];
+
+export default gameRoutes;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
+// 公共路由
 import commonRoutes from './common-routes';
+// 游戏路由
+import gameRoutes from './game-routes';
+// 其他路由
+import otherRoutes from './other-routes';
 
-// import authRoutes from './auth-routes';
-
-// import adminRoutes from './admin-routes';
-
-export default [...commonRoutes];
+export default [...commonRoutes, ...gameRoutes, ...otherRoutes];
