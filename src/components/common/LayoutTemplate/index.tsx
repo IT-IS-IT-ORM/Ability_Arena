56012b5fa7c1ede0032e1cb2c19c1b02527b2b4d
YernarT <toktaryernar@gmail.com>
2023-04-19 17:22:09 +0600

    update: :hammer: sider 显示逻辑

--- src/components/common/LayoutTemplate/index.tsx
+++ src/components/common/LayoutTemplate/index.tsx
@@ -1,15 +1,29 @@
+// React & Router
+import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+
 // 内置组件
 import Sider from './Sider';
 import PageContent from './PageContent';
+
+// Routes
+import gameRoutes from '@/routes/game-routes';
 
 // Scoped style
 import classes from './style.module.scss';
 
 export default function LayoutTemplate() {
+	const location = useLocation();
+
+	const inGame = useMemo(
+		() => gameRoutes.some(({ path }) => location.pathname === path),
+		[location],
+	);
+
 	return (
 		<div className={classes.layout}>
 			<div className="box">
-				<Sider />
+				{!inGame && <Sider />}
 				<PageContent />
 			</div>
 		</div>
