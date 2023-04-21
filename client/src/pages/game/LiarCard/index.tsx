9b251cf96c66229ce7dd31364c0a1b7855efe072
YernarT <toktaryernar@gmail.com>
2023-04-21 22:24:18 +0600

    wip: :construction: 开发中...

--- client/src/pages/game/LiarCard/index.tsx
+++ client/src/pages/game/LiarCard/index.tsx
@@ -1,3 +1,16 @@
-export default function LiarCard() {
-	return <div>LiarCard</div>;
-}
+// React
+import { memo } from 'react';
+
+// Hooks
+import { useWebSocket } from 'ahooks';
+
+// Scoped style
+import classes from './style.module.scss';
+
+export default memo(function LiarCard() {
+	const { sendMessage, latestMessage } = useWebSocket('ws://127.0.0.1:3000');
+
+	console.log('latestMessage: ', latestMessage);
+
+	return <div className={classes.liarCard}></div>;
+});
