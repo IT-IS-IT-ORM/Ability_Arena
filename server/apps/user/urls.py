f1b586a5f520586d7c1e557fd9e78fa025b1a724
YernarT <toktaryernar@gmail.com>
2023-04-21 22:27:48 +0600

    init: :rainbow: 初始化 项目

---
+++ server/apps/user/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from rest_framework import routers
+from django.conf import settings
+
+from user.views import UserViewSet, LoginAPIView
+
+if settings.DEBUG:
+    router = routers.DefaultRouter()
+else:
+    router = routers.SimpleRouter()
+
+router.register(r'user', UserViewSet)
+
+urlpatterns = router.urls
+
+urlpatterns += [ 
+    # 登录 接口
+    path('auth/login/', LoginAPIView.as_view()),
+]
+
+app_name = 'user'
