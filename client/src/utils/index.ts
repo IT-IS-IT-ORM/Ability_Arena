b5c8c6d2ec968cd5876babc0f13d34010b9b0e22
YernarT <toktaryernar@gmail.com>
2023-07-29 18:43:18 +0600

    update: :hammer: 工具函数补充

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,9 +1,13 @@
-export { default as sleep } from './sleep';
+export { default as sleep } from "./sleep";
 
-export type { localStorageProperties } from '@/utils/localStorage';
-export { localStorage } from '@/utils/localStorage';
+export type { localStorageProperties } from "@/utils/localStorage";
+export { localStorage } from "@/utils/localStorage";
 
-export type { allowedRole } from '@/utils/role';
-export { role } from '@/utils/role';
+export type { allowedRole } from "@/utils/role";
+export { role } from "@/utils/role";
 
-export { default as getAntdLocale } from './getAntdLocale';
+export { default as getAntdLocale } from "./getAntdLocale";
+
+export { convertKeysCase } from "./convertKeysCase";
+
+export { objectToUrlParams } from "./objectToParams";
