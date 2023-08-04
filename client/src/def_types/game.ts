c73bdc6985bc184dd5e9f4c70acbdc8ec33c6040
YernarT <toktaryernar@gmail.com>
2023-08-04 22:50:31 +0600

    update: :hammer: 更新类型

--- client/src/def_types/game.ts
+++ client/src/def_types/game.ts
@@ -1,6 +1,10 @@
+type ID = number;
+
 export interface I_Room {
-  id: number;
+  id: ID;
   name: string;
   maxMemberCount: number;
+  homeowner: ID;
   password?: string;
+  member: { id: ID; member: ID }[];
 }
