57ad3fe938ab80e88b31306bf8b4fb73638c0b70
YernarT <toktaryernar@gmail.com>
2023-08-05 16:24:11 +0600

    update: :hammer: 更新头像列表

--- client/src/pages/common/SettingsPage/index.tsx
+++ client/src/pages/common/SettingsPage/index.tsx
@@ -1,163 +1,183 @@
 // 全局状态
-import { useRecoilState } from 'recoil';
-import { A_User, A_Page } from '@/store';
+import { useRecoilState } from "recoil";
+import { A_User, A_Page } from "@/store";
 
 // i18n
-import i18next from 'i18next';
-import { useTranslation } from 'react-i18next';
+import i18next from "i18next";
+import { useTranslation } from "react-i18next";
 
 // Hooks
-import { useBoolean, useMemoizedFn } from 'ahooks';
+import { useBoolean, useMemoizedFn } from "ahooks";
 
 // 工具库
-import { localStorage } from '@/utils';
+import { localStorage } from "@/utils";
 
 // 图标库
-import { BiUser } from 'react-icons/bi';
-import { IoLanguage } from 'react-icons/io5';
+import { BiUser } from "react-icons/bi";
+import { IoLanguage } from "react-icons/io5";
 // Antd 组件库
-import { Input, Select } from 'antd';
+import { Input, Select } from "antd";
 // 自定义组件
-import { Button } from '@/components/common';
+import { Button } from "@/components/common";
 
 // Scoped style
-import classes from './style.module.scss';
+import classes from "./style.module.scss";
 
 // 静态资源
 import {
-	Avatar_1,
-	Avatar_2,
-	Avatar_3,
-	Avatar_4,
-	Avatar_5,
-	Avatar_6,
-	Avatar_7,
-	Avatar_8,
-	Avatar_9,
-} from '@/assets/image/user';
+  Avatar_1,
+  Avatar_2,
+  Avatar_3,
+  Avatar_4,
+  Avatar_5,
+  Avatar_6,
+  Avatar_7,
+  Avatar_8,
+  Avatar_9,
+  Avatar_10,
+  Avatar_11,
+  Avatar_12,
+  Avatar_13,
+  Avatar_14,
+  Avatar_15,
+  Avatar_16,
+  Avatar_17,
+  Avatar_18,
+} from "@/assets/image/user";
 
 const avatarList = [
-	Avatar_1,
-	Avatar_2,
-	Avatar_3,
-	Avatar_4,
-	Avatar_5,
-	Avatar_6,
-	Avatar_7,
-	Avatar_8,
-	Avatar_9,
+  Avatar_1,
+  Avatar_2,
+  Avatar_3,
+  Avatar_4,
+  Avatar_5,
+  Avatar_6,
+  Avatar_7,
+  Avatar_8,
+  Avatar_9,
+  Avatar_10,
+  Avatar_11,
+  Avatar_12,
+  Avatar_13,
+  Avatar_14,
+  Avatar_15,
+  Avatar_16,
+  Avatar_17,
+  Avatar_18,
 ];
 
 const availableLanguages = [
-	{
-		label: '中文',
-		value: 'zhCN',
-	},
-	{
-		label: 'Қазақша',
-		value: 'kkKZ',
-	},
+  {
+    label: "中文",
+    value: "zhCN",
+  },
+  {
+    label: "Қазақша",
+    value: "kkKZ",
+  },
 ];
 
 const { Option } = Select;
 
 export default function SettingsPage() {
-	// 用户信息
-	const [user, setUser] = useRecoilState(A_User);
-	// 页面信息
-	const [page, setPage] = useRecoilState(A_Page);
-	// 翻译函数
-	const { t } = useTranslation();
+  // 用户信息
+  const [user, setUser] = useRecoilState(A_User);
+  // 页面信息
+  const [page, setPage] = useRecoilState(A_Page);
+  // 翻译函数
+  const { t } = useTranslation();
 
-	// 头像列表
-	const [
-		showAvatarGrid,
-		{ setFalse: closeAvatarGrid, toggle: toggleAvatarGrid },
-	] = useBoolean(false);
+  // 头像列表
+  const [
+    showAvatarGrid,
+    { setFalse: closeAvatarGrid, toggle: toggleAvatarGrid },
+  ] = useBoolean(false);
 
-	// 处理切换头像
-	const handleAvatarChange = (index: number) => {
-		if (index !== user.avatarIdx) {
-			setUser(prevState => ({ ...prevState, avatarIdx: index }));
-		}
+  // 处理切换头像
+  const handleAvatarChange = (index: number) => {
+    if (index !== user.avatarIdx) {
+      setUser((prevState) => ({ ...prevState, avatarIdx: index }));
+    }
 
-		closeAvatarGrid();
-	};
+    closeAvatarGrid();
+  };
 
-	const handleUsernameChange = useMemoizedFn(({ target: { value } }) => {
-		setUser(prevUser => ({ ...prevUser, username: value }));
-	});
+  const handleUsernameChange = useMemoizedFn(({ target: { value } }) => {
+    setUser((prevUser) => ({ ...prevUser, username: value }));
+  });
 
-	const handleLanguageChange = useMemoizedFn(value => {
-		i18next.changeLanguage(value);
+  const handleLanguageChange = useMemoizedFn((value) => {
+    i18next.changeLanguage(value);
 
-		let newPageState;
-		setPage(prevPage => {
-			newPageState = {
-				...prevPage,
-				locale: value,
-			};
+    let newPageState;
+    setPage((prevPage) => {
+      newPageState = {
+        ...prevPage,
+        locale: value,
+      };
 
-			return newPageState;
-		});
-		localStorage.set('page', newPageState);
-	});
+      return newPageState;
+    });
+    localStorage.set("page", newPageState);
+  });
 
-	return (
-		<main
-			className={`${classes.settingsPage} ${
-				showAvatarGrid ? `${classes.settingsPage}--show` : ''
-			}`}>
-			<div className="head">
-				<div className="avatar-wrap">
-					<img src={avatarList[user.avatarIdx]} alt="avatar" />
-				</div>
-				<Button onClick={toggleAvatarGrid}>
-					{t('SettingsPage__changeAvatar') as string}
-				</Button>
-			</div>
+  return (
+    <main
+      className={`${classes.settingsPage} ${
+        showAvatarGrid ? `${classes.settingsPage}--show` : ""
+      }`}
+    >
+      <div className="head">
+        <div className="avatar-wrap">
+          <img src={avatarList[user.avatarIdx]} alt="avatar" />
+        </div>
+        <Button onClick={toggleAvatarGrid}>
+          {t("SettingsPage__changeAvatar") as string}
+        </Button>
+      </div>
 
-			<div className={`avatar-grid ${showAvatarGrid && 'avatar-grid--show'}`}>
-				{avatarList.map((avatar, idx) => (
-					<img
-						key={idx}
-						src={avatar}
-						alt="avatar"
-						onClick={() => handleAvatarChange(idx)}
-					/>
-				))}
-			</div>
+      <div className={`avatar-grid ${showAvatarGrid && "avatar-grid--show"}`}>
+        {avatarList.map((avatar, idx) => (
+          <img
+            key={idx}
+            src={avatar}
+            alt="avatar"
+            onClick={() => handleAvatarChange(idx)}
+          />
+        ))}
+      </div>
 
-			<div className={`bottom ${showAvatarGrid && 'bottom--hide'}`}>
-				<div className="group">
-					<div className="label">
-						<BiUser />
-						<span>{t('SettingsPage__username')}: </span>
-					</div>
-					<Input
-						className="field"
-						value={user.username}
-						onChange={handleUsernameChange}
-					/>
-				</div>
+      <div className={`bottom ${showAvatarGrid && "bottom--hide"}`}>
+        <div className="group">
+          <div className="label">
+            <BiUser />
+            <span>{t("SettingsPage__username")}: </span>
+          </div>
+          <Input
+            className="field"
+            value={user.username}
+            onChange={handleUsernameChange}
+          />
+        </div>
 
-				<div className="group">
-					<div className="label">
-						<IoLanguage />
-						<span>{t('SettingsPage__language')}: </span>
-					</div>
-					<Select
-						className="field"
-						value={page.locale}
-						onChange={handleLanguageChange}>
-						{availableLanguages.map(language => (
-							<Option key={language.value} value={language.value}>
-								{language.label}
-							</Option>
-						))}
-					</Select>
-				</div>
-			</div>
-		</main>
-	);
+        <div className="group">
+          <div className="label">
+            <IoLanguage />
+            <span>{t("SettingsPage__language")}: </span>
+          </div>
+          <Select
+            className="field"
+            value={page.locale}
+            onChange={handleLanguageChange}
+          >
+            {availableLanguages.map((language) => (
+              <Option key={language.value} value={language.value}>
+                {language.label}
+              </Option>
+            ))}
+          </Select>
+        </div>
+      </div>
+    </main>
+  );
 }
