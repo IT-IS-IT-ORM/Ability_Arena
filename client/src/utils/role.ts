8d22021e46b64298751c04d821fa9b4f58221e68
YernarT <toktaryernar@gmail.com>
2023-08-07 21:09:01 +0600

    update: :hammer: user role

--- client/src/utils/role.ts
+++ client/src/utils/role.ts
@@ -1,39 +1,37 @@
 // 角色
-export type allowedRole = 'GUEST' | 'GAMER' | 'ADMIN';
+export type allowedRole = "GUEST" | "GAMER";
 
 // 角色对象
 export const role: {
-	GUEST: 'GUEST';
-	GAMER: 'GAMER';
-	ADMIN: 'ADMIN';
+  GUEST: "GUEST";
+  GAMER: "GAMER";
 
-	// 所有角色
-	all: () => ['GUEST', 'GAMER', 'ADMIN'];
+  // 所有角色
+  all: () => ["GUEST", "GAMER"];
 
-	// 除 `roles` 以外的 所有角色
-	exclude: (roles: allowedRole | allowedRole[]) => allowedRole[];
+  // 除 `roles` 以外的 所有角色
+  exclude: (roles: allowedRole | allowedRole[]) => allowedRole[];
 } = {
-	GUEST: 'GUEST',
-	GAMER: 'GAMER',
-	ADMIN: 'ADMIN',
+  GUEST: "GUEST",
+  GAMER: "GAMER",
 
-	// 所有角色
-	all() {
-		return ['GUEST', 'GAMER', 'ADMIN'];
-	},
+  // 所有角色
+  all() {
+    return ["GUEST", "GAMER"];
+  },
 
-	// 除 `roles` 以外的 所有角色
-	exclude(roles) {
-		let allRole: allowedRole[] = ['GUEST', 'ADMIN'];
+  // 除 `roles` 以外的 所有角色
+  exclude(roles) {
+    let allRole: allowedRole[] = ["GUEST"];
 
-		if (Array.isArray(roles)) {
-			for (let role of roles) {
-				allRole = allRole.filter(r => r !== role);
-			}
+    if (Array.isArray(roles)) {
+      for (let role of roles) {
+        allRole = allRole.filter((r) => r !== role);
+      }
 
-			return allRole;
-		}
+      return allRole;
+    }
 
-		return allRole.filter(role => role !== roles);
-	},
+    return allRole.filter((role) => role !== roles);
+  },
 };
--- server/apps/user/models.py
+++ server/apps/user/models.py
@@ -8,15 +8,16 @@ class User(models.Model):
     '''User model'''
 
     username = models.CharField(max_length=10, unique=True, verbose_name='用户名')
-    password = models.CharField(max_length=254, verbose_name='密码')
+    # password = models.CharField(max_length=254, verbose_name='密码')
 
     ROLE_CHOICES = (
-        ('1', 'Gamer'),
-        ('2', 'Developer'),
+        ('1', 'GAMER'),
+        ('2', 'DEVELOPER'),
     )
-    role = models.CharField(max_length=1, choices=ROLE_CHOICES, default='1', verbose_name='角色')
-    # TODO: 默认头像是静态存放于服务器, 购买VIP功能的用户可以上传自定义头像
-    avatar = models.CharField(max_length=254, verbose_name='头像')
+    role = models.CharField(
+        max_length=1, choices=ROLE_CHOICES, default='1', verbose_name='角色')
+    # 头像储存于前端, 通过索引分配
+    avatar_index = models.CharField(max_length=2, verbose_name='头像索引')
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='注册时间')
 
     class Meta:
@@ -36,9 +37,9 @@ class User(models.Model):
         return True
 
 
-@receiver(pre_save, sender=User)
-def user_pre_save(sender, instance, **kwargs):
-    '''保存用户前将密码加密'''
-    # pbkdf2_sha256$
-    if not instance.password.startswith('pbkdf2_sha256$'):
-        instance.password = make_password(instance.password)
+# @receiver(pre_save, sender=User)
+# def user_pre_save(sender, instance, **kwargs):
+#     '''保存用户前将密码加密'''
+#     # pbkdf2_sha256$
+#     if not instance.password.startswith('pbkdf2_sha256$'):
+#         instance.password = make_password(instance.password)
