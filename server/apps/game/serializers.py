cca4a7f033f167f1a117264a42983f06037fc7b4
YernarT <toktaryernar@gmail.com>
2023-08-08 23:36:55 +0600

    update: :hammer: 房间 api

---
+++ server/apps/game/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from user.models import User
+from game.models import Room, RoomMember
+
+from utils.custom_exception import CustomException
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(label='ID', read_only=True)
+    name = serializers.CharField(label='房间名称', max_length=20)
+    homeowner = serializers.PrimaryKeyRelatedField(
+        label='房主', queryset=User.objects.all())
+    password = serializers.CharField(label='房间密码', required=False, max_length=8, write_only=True)
+    max_member_count = serializers.IntegerField(label='最大人数', min_value=2, max_value=10)
+
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+    def add_member(self, room_json):
+        members = RoomMember.objects.filter(room=room_json['id'])
+        room_json['member'] = RoomMemberSerializer(
+            instance=members, many=True).data
+
+        return room_json
+
+    def to_representation(self, instance):
+        return self.add_member(super().to_representation(instance))
+
+
+class RoomMemberSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(label='ID', read_only=True)
+    room = serializers.PrimaryKeyRelatedField(
+        label='房间', queryset=Room.objects.all(), write_only=True)
+    member = serializers.PrimaryKeyRelatedField(
+        label='成员', queryset=User.objects.all())
+
+    class Meta:
+        model = RoomMember
+        fields = '__all__'
+        
--- server/apps/game/views.py
+++ server/apps/game/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import authentication_classes
 
-# Create your views here.
+from game.serializers import RoomSerializer, RoomMemberSerializer
+from game.models import Room, RoomMember
+
+from utils.authentication import LoginRequiredAuthentication
+
+
+class RoomViewSet(ModelViewSet):
+    """
+    房间 API 类
+    """
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+    def get_authenticators(self):
+        if self.request.method not in ('GET'):
+            return [LoginRequiredAuthentication()]
