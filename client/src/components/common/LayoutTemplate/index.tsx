579452fe34d6e534a39c9be4289200812c544cd6
YernarT <toktaryernar@gmail.com>
2023-08-31 23:30:11 +0600

    wip: in progress

--- client/src/components/common/LayoutTemplate/index.tsx
+++ client/src/components/common/LayoutTemplate/index.tsx
@@ -1,48 +1,34 @@
 // React & Router
-import { useMemo, useRef } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
 // Hooks
-import { useMouse, useUpdateEffect } from 'ahooks';
+import {} from "ahooks";
 
 // 内置组件
-import Sider from './Sider';
-import PageContent from './PageContent';
+import Sider from "./Sider";
+import PageContent from "./PageContent";
 
 // Routes
-import gameRoutes from '@/routes/game-routes';
+import gameRoutes from "@/routes/game-routes";
 
 // Scoped style
-import classes from './style.module.scss';
+import classes from "./style.module.scss";
 
 export default function LayoutTemplate() {
-	const location = useLocation();
+  const location = useLocation();
 
-	const inGame = useMemo(
-		() => gameRoutes.some(({ path }) => location.pathname === path),
-		[location],
-	);
+  // const inGame = useMemo(
+  // 	() => gameRoutes.some(({ path }) => location.pathname === path),
+  // 	[location],
+  // );
+  const inGame = false;
 
-	const boxRef = useRef<HTMLDivElement>(null);
-	const { pageX, pageY } = useMouse(boxRef);
-
-	useUpdateEffect(() => {
-		const $box = boxRef.current as HTMLDivElement;
-
-		// const xTranslate = pageX;
-		// const yTranslate = pageY;
-
-		// $box.style.cssText = `
-		// transform: translate3d(${xTranslate}px, ${yTranslate}px, 0);
-		// `;
-	}, [pageX, pageY]);
-
-	return (
-		<div className={classes.layout}>
-			<div ref={boxRef} className="box">
-				{!inGame && <Sider />}
-				<PageContent />
-			</div>
-		</div>
-	);
+  return (
+    <div className={classes.layout}>
+      <div className="box">
+        {!inGame && <Sider />}
+        <PageContent />
+      </div>
+    </div>
+  );
 }
