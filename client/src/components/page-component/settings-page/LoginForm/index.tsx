939f9ea04991abf838f6d78f17f944d9131c4cd4
YernarT <toktaryernar@gmail.com>
2023-08-31 23:30:26 +0600

    init: :rainbow: Login Form

---
+++ client/src/components/page-component/settings-page/LoginForm/index.tsx
@@ -0,0 +1,15 @@
+// Antd component
+import { Form } from 'antd'
+
+// Scoped style
+import classes from './style.module.scss'
+
+export default function LoginForm() {
+  return (
+    <Form>
+        <Form.Item name=''>
+            
+        </Form.Item>
+    </Form>
+  )
+}
