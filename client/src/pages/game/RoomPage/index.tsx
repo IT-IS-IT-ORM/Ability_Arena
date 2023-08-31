58ad7f0bea8cd00c6f41dff1c0e1ef0c8788b9bd
YernarT <toktaryernar@gmail.com>
2023-08-31 23:31:44 +0600

    init: :rainbow: room page

--- client/src/pages/game/RoomPage/index.tsx
+++ client/src/pages/game/RoomPage/index.tsx
@@ -1,12 +1,41 @@
 // React
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
+// Router
+import { useParams, useHistory } from "react-router-dom";
+// Recoil
+import { useRecoilState } from "recoil";
+import { A_User } from "@/store";
 
 // Hooks
-import { useWebSocket } from "ahooks";
+import { useWebSocket, useMount } from "ahooks";
+// API
+import { WS_GameURI } from "@/service/game.ws";
 
 // Scoped style
 import classes from "./style.module.scss";
 
-export default function RoomPage() {
+export default memo(function RoomPage() {
+  const params = useParams<{ roomId: string }>();
+  const history = useHistory();
+  const [user, setUser] = useRecoilState(A_User);
+
+  // 校验 roomId
+  useMount(() => {
+    const roomId = Number.parseInt(params.roomId);
+
+    if (Number.isNaN(roomId) || roomId <= 0) {
+      history.replace("/404");
+    }
+  });
+
+  const { latestMessage, sendMessage } = useWebSocket(
+    WS_GameURI(parseInt(params.roomId), user.token),
+    {
+      reconnectLimit: 0,
+    }
+  );
+
+  console.log("latestMessage: ", latestMessage);
+
   return <div className={classes.roomPage}>room page, room id: {1}</div>;
-}
+});
