a2d3cfd8979e75f504c1f49c41fba9d0e962c5ca
YernarT <toktaryernar@gmail.com>
2023-08-31 23:29:41 +0600

    update: :hammer: game state

--- client/src/store/game-atom.ts
+++ client/src/store/game-atom.ts
@@ -1,11 +1,32 @@
-import { atom } from "recoil";
+import type { I_Room } from "@/def_types/game";
 
-export interface gameStateProperties {}
+import { atom, selector } from "recoil";
+import { A_User } from "@/store/user-atom";
 
-export const defaultGameState: gameStateProperties = {};
+export interface gameStateProperties {
+  roomList: I_Room[];
+}
+
+export const defaultGameState: gameStateProperties = {
+  roomList: [],
+};
 
 export const A_Game = atom({
   key: "A_Game",
   // default value, aka initial value
   default: defaultGameState,
+});
+
+export const S_RoomDetail = selector({
+  key: "S_RoomDetail",
+  get({ get }) {
+    const user = get(A_User);
+    const game = get(A_Game);
+
+    return game.roomList.find(
+      (room) =>
+        room.homeowner === user.id ||
+        room.member.find((record) => record.member === user.id)
+    );
+  },
 });
