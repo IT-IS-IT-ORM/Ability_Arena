76431d90992f9368bb4dc551a0bb0748e9bde623
YernarT <toktaryernar@gmail.com>
2023-09-03 23:38:12 +0600

    wip: :construction: 开发中

--- server/apps/game/consumers.py
+++ server/apps/game/consumers.py
@@ -1,80 +1,83 @@
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
-from channels.layers import get_channel_layer
+from channels.generic.websocket import JsonWebsocketConsumer
+
+from user.models import User
+from game.models import Room, RoomMember
+from game.serializers import RoomSerializer, RoomMemberSerializer
 
 from utils.jwt import check_jwt
 
-from json import loads as json_loads, dumps as json_dumps
+from asgiref.sync import async_to_sync
 
 
-class GameConsumer(AsyncJsonWebsocketConsumer):
+class GameConsumer(JsonWebsocketConsumer):
 
-    async def connect(self):
+    def connect(self):
         self.room_id = self.scope['url_route']['kwargs']['room_id']
         self.room_group_name = f'game-{self.room_id}'
 
         print('建立WS链接')
-        await self.accept()
+        self.accept()
 
-        # set user (`None` or `instance`)
-        # self.token = self.scope.get(
-        #     "query_string", b"token=").decode("utf-8").split('=')[-1]
-        # is_valid, result = check_jwt(self.token)
-        # self.user = result if is_valid else None
+        self.token = self.scope.get(
+            "query_string", b"token=").decode("utf-8").split('=')[-1]
+        is_valid, result = check_jwt(self.token)
+        self.user = result if is_valid else None
 
-        # if self.user is None:
-        #     await self.chat_message({'message': '你需要登录'})
-        #     await self.close()
-        #     return
-        
-        # # Join room group
-        # print('加入房间')
-        # await self.channel_layer.group_add(
-        #     self.room_group_name,
-        #     self.channel_name
-        # )
-        # await self.channel_layer.group_send(
-        #     self.room_group_name,
-        #     {
-        #         "type": "chat.message",
-        #         "room_id": self.room_id,
-        #         # "username": self.scope["user"].username,
-        #         "username": 'Test user',
-        #         "message": 'a new person join',
-        #     }
-        # )
+        if self.user is None:
+            self.chat_message({'message': '请先登录!'})
+            self.close()
+            return
 
-    async def disconnect(self, close_code):
-        print('退出房间')
-        # Leave room group
-        await self.channel_layer.group_discard(
+        # TODO: 如何掉线重连?
+
+        try:
+            self.room = Room.objects.get(id=int(self.room_id))
+        except Room.DoesNotExist:
+            self.chat_message({'message': '房间不存在!'})
+            self.close()
+            return
+
+        print('加入房间')
+        self.room.add_member(self.room, self.user)
+        self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        print('断开WS链接')
 
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json_loads(text_data)
-        message = text_data_json['message']
-
-        print('self.groups: ', self.room_group_name)
-
-        print('ring size: ', self.channel_layer.ring_size)
-
-        # Send message to room group
-        await self.channel_layer.group_send(
+        print('通知所有人，有人加入房间')
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                'type': 'chat_message',
-                'message': message
+                "type": "chat.message",
+                "room_id": self.room_id,
+                "username": self.user.username,
+                "message": 'a new person join',
             }
         )
 
-    # Receive message from room group
-    async def chat_message(self, event):
-        message = '疯狂三国杀：' + event['message']
+    def disconnect(self, close_code):
+        print('退出房间')
+        self.room.discard_member(self.room, self.user)
+        self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "chat.message",
+                "room_id": self.room_id,
+                "username": self.user.username,
+                "message": '有人退出了',
+            }
+        )
+        print('断开WS链接')
 
-        # Send message to WebSocket
-        await self.send_json({
+    def receive_json(self, content):
+        print('receive_json: ', content)
+        self.chat_message(content)
+
+    def chat_message(self, message):
+        self.send_json({
             'message': message
         })
