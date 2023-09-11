d0f57dda0fbb6cf00524be89decb572488ee88f9
YernarT <toktaryernar@gmail.com>
2023-09-11 23:06:09 +0600

    feat: :sparkles: docs page

--- client/src/components/common/LayoutTemplate/Sider/NavbarList/index.tsx
+++ client/src/components/common/LayoutTemplate/Sider/NavbarList/index.tsx
@@ -1,66 +1,71 @@
 // React 路由
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 // 全局状态
-import { useRecoilValue } from 'recoil';
-import { S_UserIsAuthenticated } from '@/store';
+import { useRecoilValue } from "recoil";
+import { S_UserIsAuthenticated } from "@/store";
 
 // i18n
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 // 图标库
-import { BiHomeSmile } from 'react-icons/bi';
-import { GrGamepad } from 'react-icons/gr';
-import { AiOutlineSetting } from 'react-icons/ai';
+import { BiHomeSmile } from "react-icons/bi";
+import { GrGamepad,GrBook } from "react-icons/gr";
+import { AiOutlineSetting } from "react-icons/ai";
 // 自定义组件
-import { Button } from '@/components/common';
+import { Button } from "@/components/common";
 
 // Scoped style
-import classes from './style.module.scss';
+import classes from "./style.module.scss";
 
 const navbarItems = [
-	{
-		to: '/',
-		icon: <BiHomeSmile />,
-		text: 'Navigator__home',
-	},
-	{
-		to: '/games',
-		icon: <GrGamepad />,
-		text: 'Navigator__games',
-	},
-	{
-		to: '/settings',
-		icon: <AiOutlineSetting />,
-		text: 'Navigator__settings',
-	},
+  {
+    to: "/",
+    icon: <BiHomeSmile />,
+    text: "Navigator__home",
+  },
+  {
+    to: "/games",
+    icon: <GrGamepad />,
+    text: "Navigator__games",
+  },
+  {
+    to: "/docs",
+    icon: <GrBook />,
+    text: "Navigator__docs",
+  },
+  {
+    to: "/settings",
+    icon: <AiOutlineSetting />,
+    text: "Navigator__settings",
+  },
 ];
 
 export default function NavbarList() {
-	// i18n
-	const { t } = useTranslation();
-	// 路由变量
-	const { push } = useHistory();
-	// 是否授权
-	const isAuthenticated = useRecoilValue(S_UserIsAuthenticated);
+  // i18n
+  const { t } = useTranslation();
+  // 路由变量
+  const { push } = useHistory();
+  // 是否授权
+  const isAuthenticated = useRecoilValue(S_UserIsAuthenticated);
 
-	// 按钮点击事件
-	const handleNavItemClick = (targetPath: string) => {
-		push(targetPath);
-	};
+  // 按钮点击事件
+  const handleNavItemClick = (targetPath: string) => {
+    push(targetPath);
+  };
 
-	return (
-		<ul className={classes.navbarList}>
-			{navbarItems.map((item, idx) => (
-				<li key={item.to} className={classes.navbarItem}>
-					<Button onClick={() => handleNavItemClick(item.to)}>
-						<>
-							{item.icon}
-							{t(item.text) as string}
-						</>
-					</Button>
-				</li>
-			))}
-		</ul>
-	);
+  return (
+    <ul className={classes.navbarList}>
+      {navbarItems.map((item, idx) => (
+        <li key={item.to} className={classes.navbarItem}>
+          <Button onClick={() => handleNavItemClick(item.to)}>
+            <>
+              {item.icon}
+              {t(item.text) as string}
+            </>
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
 }
