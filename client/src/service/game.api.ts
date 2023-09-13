7c85113805869ff84d215e3fb3e79fa1ef88bca6
YernarT <toktaryernar@gmail.com>
2023-09-14 00:14:03 +0600

    update: :hammer: 获取房间信息

--- client/src/service/game.api.ts
+++ client/src/service/game.api.ts
@@ -10,5 +10,8 @@ export interface API_GetRoomsParams {
 export const API_GetRooms = (params?: API_GetRoomsParams) =>
   fetch.get<I_Room[]>("/room/", params);
 
+export const API_GetRoom = (roomId: number) =>
+  fetch.get<I_Room>(`/room/${roomId}/`);
+
 export const API_CreateRoom = (data: Omit<I_Room, "member">) =>
   fetch.post<I_Room>("/room/", data);
