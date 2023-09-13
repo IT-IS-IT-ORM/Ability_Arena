24212699bcc3c438c39912a159235836fe45b2bb
YernarT <toktaryernar@gmail.com>
2023-09-14 00:13:42 +0600

    update: :hammer: 校验房间

--- client/src/pages/game/RoomPage/index.tsx
+++ client/src/pages/game/RoomPage/index.tsx
@@ -4,44 +4,85 @@ import { memo, useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
 // Recoil
 import { useRecoilState } from "recoil";
-import { A_User } from "@/store";
+import { A_User, A_Game } from "@/store";
 
+// i18n
+import { useTranslation } from "react-i18next";
 // Hooks
-import { useWebSocket, useMount } from "ahooks";
-// API
+import { useWebSocket, useMount, useRequest } from "ahooks";
+// Service
+import { API_GetRoom } from "@/service/game.api";
 import { WS_GameURI } from "@/service/game.ws";
+
+// Antd Component
+import { message as AntdMessage } from "antd";
 // Custom Component
-import { ChatBlock } from "@/components/page-component/games-page";
+import { Button } from "@/components/common";
+import {
+  ChatBlock,
+  RoomMemberList,
+} from "@/components/page-component/games-page";
 
 // Scoped style
 import classes from "./style.module.scss";
 
 export default memo(function RoomPage() {
+  const { t } = useTranslation();
   const params = useParams<{ roomId: string }>();
   const history = useHistory();
   const [user, setUser] = useRecoilState(A_User);
+  const [game, setGame] = useRecoilState(A_Game);
+
+  const { runAsync: getRoom, loading: getRoomLoading } = useRequest(
+    API_GetRoom,
+    { manual: true }
+  );
+
+  const { connect, disconnect, latestMessage, sendMessage } = useWebSocket(
+    WS_GameURI(parseInt(params.roomId), user.token),
+    {
+      manual: true,
+      reconnectLimit: 0,
+    }
+  );
 
   // 校验 roomId
-  useMount(() => {
+  useMount(async () => {
     const roomId = Number.parseInt(params.roomId);
 
     if (Number.isNaN(roomId) || roomId <= 0) {
       history.replace("/404");
+      return;
     }
-  });
 
-  const { latestMessage, sendMessage } = useWebSocket(
-    WS_GameURI(parseInt(params.roomId), user.token),
-    {
-      reconnectLimit: 0,
+    const response = await getRoom(roomId).catch((err) => {
+      AntdMessage.error(t(err.message));
+    });
+
+    if (response && response.isOk) {
+      setGame((prevGame) => ({ ...prevGame, currentRoom: response.data }));
+      connect?.();
+      return;
     }
-  );
+
+    history.replace("/404");
+    return;
+  });
 
   console.log("latestMessage: ", latestMessage);
 
   return (
     <div className={classes.roomPage}>
-      <ChatBlock room={{ name: "abcdefgh" }} />
+      {!getRoomLoading && (
+        <>
+          <ChatBlock className="chat-block" />
+          <RoomMemberList className="room-member-list" />
+          <div className="room-setting">
+            <Button block>房间设置</Button>
+            <Button block>开始游戏</Button>
+          </div>
+        </>
+      )}
     </div>
   );
 });
