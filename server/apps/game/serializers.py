4a9d4a68cf2d532f2e6895ac7c786fbc53094448
YernarT <toktaryernar@gmail.com>
2023-09-14 00:14:34 +0600

    fix: :bug: 返回格式

--- server/apps/game/serializers.py
+++ server/apps/game/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from user.models import User
+from user.serializers import UserSerializer
 from game.models import Room, RoomMember
 
 from utils.custom_exception import CustomException
@@ -10,8 +11,10 @@ class RoomSerializer(serializers.ModelSerializer):
     name = serializers.CharField(label='房间名称', max_length=20)
     homeowner = serializers.PrimaryKeyRelatedField(
         label='房主', queryset=User.objects.all())
-    password = serializers.CharField(label='房间密码', required=False, max_length=8, write_only=True)
-    max_member_count = serializers.IntegerField(label='最大人数', min_value=2, max_value=10)
+    password = serializers.CharField(
+        label='房间密码', required=False, max_length=8, write_only=True)
+    max_member_count = serializers.IntegerField(
+        label='最大人数', min_value=2, max_value=10)
 
     class Meta:
         model = Room
@@ -20,7 +23,7 @@ class RoomSerializer(serializers.ModelSerializer):
     def add_member(self, room_json):
         members = RoomMember.objects.filter(room=room_json['id'])
         # TODO: 这里直接返回User
-        room_json['member'] = RoomMemberSerializer(
+        room_json['member_list'] = RoomMemberSerializer(
             instance=members, many=True).data
 
         return room_json
@@ -33,10 +36,8 @@ class RoomMemberSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(label='ID', read_only=True)
     room = serializers.PrimaryKeyRelatedField(
         label='房间', queryset=Room.objects.all(), write_only=True)
-    member = serializers.PrimaryKeyRelatedField(
-        label='成员', queryset=User.objects.all())
+    member = UserSerializer()
 
     class Meta:
         model = RoomMember
         fields = '__all__'
-        
