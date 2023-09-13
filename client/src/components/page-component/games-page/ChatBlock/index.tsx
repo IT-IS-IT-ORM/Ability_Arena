5b436d8b338f73b4876c3604eed37f35333f70ed
YernarT <toktaryernar@gmail.com>
2023-09-13 12:20:32 +0600

    feat: :sparkles: chat block

--- client/src/components/page-component/games-page/ChatBlock/index.tsx
+++ client/src/components/page-component/games-page/ChatBlock/index.tsx
@@ -1,10 +1,19 @@
 // Types
 import type { I_Room } from "@/def_types/game";
 
+// React
+import { useMemo } from "react";
+// Recoil
+import { useRecoilValue } from "recoil";
+import { A_User } from "@/store";
+
 // Icon
 import { BiArrowBack } from "react-icons/bi";
+// Antd Component
+import { Input } from "antd";
 // Custom Component
 import { Button } from "@/components/common";
+import { ChatMessage } from "@/components/page-component/games-page";
 
 // Scoped style
 import classes from "./style.module.scss";
@@ -15,6 +24,53 @@ interface ChatBlockProps {
 }
 
 export default function ChatBlock({ room }: ChatBlockProps) {
+  const messages = [
+    {
+      messageType: "chat.info",
+      messageContent: "Gamer1234 加入了房间",
+      messageTime: "2023-09-13 10:10:10",
+    },
+    {
+      messageType: "chat.message",
+      messageContent: "哇，终于可以玩游戏了，我先等一下其他玩家加入~",
+      messageTime: "2023-09-13 10:10:16",
+      sender: {
+        id: 1,
+        username: "Gamer1234",
+        avatarIndex: 24,
+      },
+    },
+    {
+      messageType: "chat.info",
+      messageContent: "GamerXxx 加入了房间",
+      messageTime: "2023-09-13 10:10:23",
+    },
+    {
+      messageType: "chat.message",
+      messageContent: "Hi! 你是萌新吗?",
+      messageTime: "2023-09-13 10:10:29",
+      sender: {
+        id: 1,
+        username: "Gamer1234",
+        avatarIndex: 24,
+      },
+    },
+    {
+      messageType: "chat.message",
+      messageContent: "没错, 第一次玩疯狂三国杀",
+      messageTime: "2023-09-13 10:10:48",
+      sender: {
+        id: 2,
+        username: "GamerXxx",
+        avatarIndex: 29,
+      },
+    },
+  ];
+
+  const user = useRecoilValue(A_User);
+
+  const isMyRoom = useMemo(() => room.homeowner === user.id, [room, user]);
+
   return (
     <section className={classes.chatBlock}>
       <div className="head">
@@ -25,10 +81,20 @@ export default function ChatBlock({ room }: ChatBlockProps) {
       </div>
 
       <div className="chat">
-        
+        {messages.map((message) => (
+          <ChatMessage isMyRoom={isMyRoom} {...message} />
+        ))}
       </div>
 
-      <div className="enter"></div>
+      <div className="enter">
+        <Input.TextArea
+          showCount
+          rows={3}
+          maxLength={128}
+          placeholder="输入你的消息..."
+        />
+        <Input placeholder="输入你的消息..." />
+      </div>
     </section>
   );
 }
