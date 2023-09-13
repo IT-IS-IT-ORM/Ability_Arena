4438e786834e746b27e15b5e997667df5789ce8a
YernarT <toktaryernar@gmail.com>
2023-09-14 00:14:18 +0600

    update: :hammer: 数据共享

--- client/src/store/game-atom.ts
+++ client/src/store/game-atom.ts
@@ -1,14 +1,26 @@
-import type { I_Room } from "@/def_types/game";
+import type { I_Room, I_WS_Message } from "@/def_types/game";
+import type { I_User } from "@/def_types/user";
 
 import { atom, selector } from "recoil";
 import { A_User } from "@/store/user-atom";
 
 export interface gameStateProperties {
+  currentRoom: I_Room | null;
   roomList: I_Room[];
+  // 聊天室内容
+  messageList: I_WS_Message<
+    | I_User
+    | {
+        content: string;
+        sender: I_User;
+      }
+  >[];
 }
 
 export const defaultGameState: gameStateProperties = {
+  currentRoom: null,
   roomList: [],
+  messageList: [],
 };
 
 export const A_Game = atom({
