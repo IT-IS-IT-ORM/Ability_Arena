2e5b510824df6c1cd3e04ef191d3d55b19c14dd5
YernarT <toktaryernar@gmail.com>
2023-09-14 00:13:19 +0600

    feat: :sparkles: 玩家列表

--- client/src/components/page-component/games-page/index.ts
+++ client/src/components/page-component/games-page/index.ts
@@ -4,3 +4,4 @@ export { default as CreateRoomModal } from "./CreateRoomModal";
 
 export { default as ChatBlock } from "./ChatBlock";
 export { default as ChatMessage } from "./ChatMessage";
+export { default as RoomMemberList } from "./RoomMemberList";
