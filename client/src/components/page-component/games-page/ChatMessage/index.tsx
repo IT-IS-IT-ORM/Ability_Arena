0e03e2d8828f8fc64f539199c5fdb61dd24f0ade
YernarT <toktaryernar@gmail.com>
2023-09-14 00:12:20 +0600

    wip: :construction: 聊天消息

--- client/src/components/page-component/games-page/ChatMessage/index.tsx
+++ client/src/components/page-component/games-page/ChatMessage/index.tsx
@@ -1,5 +1,6 @@
 // Types
 import type { I_User } from "@/def_types/user";
+import type { I_WS_Message } from "@/def_types/game";
 
 // Utils
 import { getAvatar } from "@/utils";
@@ -10,28 +11,34 @@ import classes from "./style.module.scss";
 // Props
 type T_MessageType = "chat.info" | "chat.message";
 
-interface ChatMessageProps {
+interface ChatMessageProps
+  extends I_WS_Message<
+    | I_User
+    | {
+        content: string;
+        sender: I_User;
+      }
+  > {
   isMyRoom: boolean;
-  messageType: T_MessageType;
-  messageContent: string;
-  messageTime: string;
-  sender?: I_User;
 }
 
 export default function ChatMessage({
   isMyRoom,
+  messageId,
   messageType,
-  messageContent,
   messageTime,
-  sender,
+  data,
 }: ChatMessageProps) {
+  console.log("chatMessage data: ", data);
+
   if (messageType === "chat.info") {
     return (
       <div className={classes.chatInfo}>
         <span className="time">{messageTime}</span>
         <span className="content">
           {isMyRoom && "(你)"}
-          {messageContent}
+          {/* {data} */}
+          加入了房间
         </span>
       </div>
     );
@@ -40,18 +47,18 @@ export default function ChatMessage({
   return (
     <div className={classes.chatMessage}>
       <img
-        src={getAvatar((sender as I_User).avatarIndex)}
+        // src={getAvatar((sender as I_User).avatarIndex)}
         className="avatar"
         alt="Avatar"
       />
 
       <div className="block">
         <div className="row">
-          <span className="name">{sender?.username}</span>
+          {/* <span className="name">{sender?.username}</span> */}
           <span className="time">{messageTime}</span>
         </div>
 
-        <span>{messageContent}</span>
+        {/* <span>{messageContent}</span> */}
       </div>
     </div>
   );
