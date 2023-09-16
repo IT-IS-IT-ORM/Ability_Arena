98d03e9b8600fa41a88002ce35309cbd0efad0e2
YernarT <toktaryernar@gmail.com>
2023-09-16 23:54:10 +0600

    update: :hammer: 泛型

--- client/src/utils/convertKeysCase.ts
+++ client/src/utils/convertKeysCase.ts
@@ -1,17 +1,21 @@
 import _ from "lodash";
 
-export function convertKeysCase(
-  object: object,
+export function convertKeysCase<T=object>(
+  object: T,
   case_: "camel" | "snake"
-): object {
+): T {
   if (Array.isArray(object)) {
-    return object.map((item) => convertKeysCase(item, case_));
+    return object.map((item) => convertKeysCase(item, case_)) as T;
+  }
+
+  if (typeof object === 'string') {
+    return _[`${case_}Case`](object) as T;
   }
 
   if (!_.isNumber(object)) {
     const newObj = {};
 
-    _.forEach(object, (value, key) => {
+    _.forEach(object as T[], (value, key) => {
       if (_.isObject(value)) {
         // @ts-ignore
         newObj[_[`${case_}Case`](key)] = convertKeysCase(value, case_);
@@ -21,7 +25,7 @@ export function convertKeysCase(
       }
     });
 
-    return newObj;
+    return newObj as T;
   }
 
   return object;
