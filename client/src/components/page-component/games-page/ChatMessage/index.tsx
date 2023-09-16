3c4b5a6114b32408e06fee35b03c09568a693649
YernarT <toktaryernar@gmail.com>
2023-09-17 01:29:26 +0600

    update: :hammer: 修改类型

--- client/src/components/page-component/games-page/ChatMessage/index.tsx
+++ client/src/components/page-component/games-page/ChatMessage/index.tsx
@@ -1,44 +1,47 @@
 // Types
 import type { I_User } from "@/def_types/user";
-import type { I_WS_Message } from "@/def_types/game";
+import type { I_WS_ChatMessage, I_PlaerMessage } from "@/def_types/game";
 
 // Utils
 import { getAvatar } from "@/utils";
+// Hooks
+import { useCreation } from "ahooks";
 
 // Scoped style
 import classes from "./style.module.scss";
 
 // Props
-type T_MessageType = "chat.info" | "chat.message";
-
-interface ChatMessageProps
-  extends I_WS_Message<
-    | I_User
-    | {
-        content: string;
-        sender: I_User;
-      }
-  > {
+interface ChatMessageProps {
   isMyRoom: boolean;
+  message: I_WS_ChatMessage;
 }
 
 export default function ChatMessage({
   isMyRoom,
-  messageId,
-  messageType,
-  messageTime,
-  data,
+  message: { messageTime, messageType, data },
 }: ChatMessageProps) {
-  console.log("chatMessage data: ", data);
+  // 格式化日期时间
+  const formattedDate = useCreation(() => {
+    const date = new Date(messageTime);
+    const year = date.getFullYear();
+    // 获取月份（需要加1，因为月份从0开始，然后使用padStart函数补0）
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
 
-  if (messageType === "chat.info") {
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }, [messageTime]);
+
+  console.log("isMyRoom? ", isMyRoom, data);
+
+  if (messageType[1] === "info") {
     return (
       <div className={classes.chatInfo}>
-        <span className="time">{messageTime}</span>
+        <span className="time">{formattedDate}</span>
         <span className="content">
-          {isMyRoom && "(你)"}
-          {/* {data} */}
-          加入了房间
+          {isMyRoom ? "你" : (data as I_User).username} 加入了房间
         </span>
       </div>
     );
@@ -47,18 +50,20 @@ export default function ChatMessage({
   return (
     <div className={classes.chatMessage}>
       <img
-        // src={getAvatar((sender as I_User).avatarIndex)}
+        src={getAvatar((data as I_PlaerMessage).sender.avatarIndex)}
         className="avatar"
         alt="Avatar"
       />
 
       <div className="block">
         <div className="row">
-          {/* <span className="name">{sender?.username}</span> */}
-          <span className="time">{messageTime}</span>
+          <span className="name">
+            {(data as I_PlaerMessage).sender.username}
+          </span>
+          <span className="time">{formattedDate}</span>
         </div>
 
-        {/* <span>{messageContent}</span> */}
+        <span>{(data as I_PlaerMessage).content}</span>
       </div>
     </div>
   );
