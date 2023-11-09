5821d5a74c0e5b2ae21e842f2d66cee46bb26ac7
YernarT <toktaryernar@gmail.com>
2023-11-10 01:54:37 +0600

    update: 导航更新

--- client/src/components/common/LayoutTemplate/Sider/NavbarList/index.tsx
+++ client/src/components/common/LayoutTemplate/Sider/NavbarList/index.tsx
@@ -7,46 +7,25 @@ import { S_UserIsAuthenticated } from "@/store";
 
 // i18n
 import { useTranslation } from "react-i18next";
+// Hooks
+import { useCreation } from "ahooks";
 
 // 图标库
 import { BiHomeSmile } from "react-icons/bi";
-import { GrGamepad,GrBook } from "react-icons/gr";
-import { AiOutlineSetting } from "react-icons/ai";
+import { GrGamepad, GrBook } from "react-icons/gr";
+import { AiOutlineSetting, AiOutlineLogin } from "react-icons/ai";
 // 自定义组件
 import { Button } from "@/components/common";
 
 // Scoped style
 import classes from "./style.module.scss";
 
-const navbarItems = [
-  {
-    to: "/",
-    icon: <BiHomeSmile />,
-    text: "Navigator__home",
-  },
-  {
-    to: "/games",
-    icon: <GrGamepad />,
-    text: "Navigator__games",
-  },
-  {
-    to: "/docs",
-    icon: <GrBook />,
-    text: "Navigator__docs",
-  },
-  {
-    to: "/settings",
-    icon: <AiOutlineSetting />,
-    text: "Navigator__settings",
-  },
-];
-
 export default function NavbarList() {
   // i18n
   const { t } = useTranslation();
   // 路由变量
   const { push } = useHistory();
-  // 是否授权
+  // 是否已授权
   const isAuthenticated = useRecoilValue(S_UserIsAuthenticated);
 
   // 按钮点击事件
@@ -54,9 +33,35 @@ export default function NavbarList() {
     push(targetPath);
   };
 
+  const navbarItems = useCreation(
+    () => [
+      {
+        to: "/",
+        icon: <BiHomeSmile />,
+        text: "Navigator__home",
+      },
+      {
+        to: "/games",
+        icon: <GrGamepad />,
+        text: "Navigator__games",
+      },
+      {
+        to: "/docs",
+        icon: <GrBook />,
+        text: "Navigator__docs",
+      },
+      {
+        to: "/settings",
+        icon: isAuthenticated ? <AiOutlineSetting /> : <AiOutlineLogin />,
+        text: isAuthenticated ? "Navigator__settings" : "Navigator__login",
+      },
+    ],
+    [isAuthenticated]
+  );
+
   return (
     <ul className={classes.navbarList}>
-      {navbarItems.map((item, idx) => (
+      {navbarItems.map((item) => (
         <li key={item.to} className={classes.navbarItem}>
           <Button onClick={() => handleNavItemClick(item.to)}>
             <>
--- client/src/components/common/LayoutTemplate/Sider/index.tsx
+++ client/src/components/common/LayoutTemplate/Sider/index.tsx
@@ -1,92 +1,118 @@
 // 类型
-import type { MenuProps } from 'antd';
+import type { MenuProps } from "antd";
 
 // React
-import { useRef } from 'react';
+import { useRef } from "react";
 // React 路由
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+// 全局状态
+import { useRecoilValue } from "recoil";
+import { S_UserIsAuthenticated } from "@/store";
 
 // i18n
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+// Hooks
+import { useCreation } from "ahooks";
 
 // Antd 组件库
-import { Dropdown } from 'antd';
+import { Dropdown } from "antd";
 // 图标库
-import { CgMenuGridO } from 'react-icons/cg';
-import { BiHomeSmile } from 'react-icons/bi';
-import { GrGamepad } from 'react-icons/gr';
-import { AiOutlineSetting } from 'react-icons/ai';
+import { BiHomeSmile } from "react-icons/bi";
+import { GrGamepad, GrBook } from "react-icons/gr";
+import { AiOutlineSetting, AiOutlineLogin } from "react-icons/ai";
+import { CgMenuGridO } from "react-icons/cg";
 // 自定义组件
-import { Button } from '@/components/common';
+import { Button } from "@/components/common";
 // 内置组件
-import NavbarList from './NavbarList';
+import NavbarList from "./NavbarList";
 
 // Scoped style
-import classes from './style.module.scss';
+import classes from "./style.module.scss";
 
 export default function Sider() {
-	const { t } = useTranslation();
-	const { push } = useHistory();
-	const navigatorBtnRef = useRef<HTMLDivElement>(null);
+  const { t } = useTranslation();
+  const { push } = useHistory();
+  const navigatorBtnRef = useRef<HTMLDivElement>(null);
+  // 是否已授权
+  const isAuthenticated = useRecoilValue(S_UserIsAuthenticated);
 
-	const items: MenuProps['items'] = [
-		{
-			key: '/',
-			label: (
-				<Button onClick={() => push('/')}>
-					<>
-						<BiHomeSmile />
-						<p>{t('Navigator__home')}</p>
-					</>
-				</Button>
-			),
-		},
-		{
-			key: '/games',
-			label: (
-				<Button onClick={() => push('/games')}>
-					<>
-						<GrGamepad />
-						<p>{t('Navigator__games')}</p>
-					</>
-				</Button>
-			),
-		},
-		{
-			key: '/settings',
-			label: (
-				<Button onClick={() => push('/settings')}>
-					<>
-						<AiOutlineSetting />
-						<p>{t('Navigator__settings')}</p>
-					</>
-				</Button>
-			),
-		},
-	];
+  const items: MenuProps["items"] = useCreation(
+    () => [
+      {
+        key: "/",
+        label: (
+          <Button onClick={() => push("/")}>
+            <>
+              <BiHomeSmile />
+              <p>{t("Navigator__home")}</p>
+            </>
+          </Button>
+        ),
+      },
+      {
+        key: "/games",
+        label: (
+          <Button onClick={() => push("/games")}>
+            <>
+              <GrGamepad />
+              <p>{t("Navigator__games")}</p>
+            </>
+          </Button>
+        ),
+      },
+      {
+        key: "/docs",
+        label: (
+          <Button onClick={() => push("/docs")}>
+            <>
+              <GrBook />
+              <p>{t("Navigator__docs")}</p>
+            </>
+          </Button>
+        ),
+      },
+      {
+        key: "/settings",
+        label: (
+          <Button onClick={() => push("/settings")}>
+            <>
+              {isAuthenticated ? <AiOutlineSetting /> : <AiOutlineLogin />}
+              <p>
+                {t(
+                  isAuthenticated ? "Navigator__settings" : "Navigator__login"
+                )}
+              </p>
+            </>
+          </Button>
+        ),
+      },
+    ],
+    [isAuthenticated]
+  );
 
-	return (
-		<aside className={classes.sider}>
-			<div className="brand">
-				<span>Play</span>
-				<span>ground</span>
-			</div>
+  return (
+    <aside className={classes.sider}>
+      <div className="brand">
+        <span>Play</span>
+        <span>ground</span>
+      </div>
 
-			<Dropdown
-				menu={{ items }}
-				placement="bottom"
-				getPopupContainer={() => navigatorBtnRef.current as HTMLDivElement}>
-				<div className="navigator-btn" ref={navigatorBtnRef}>
-					<CgMenuGridO />
-				</div>
-			</Dropdown>
+      <Dropdown
+        menu={{ items }}
+        placement="bottom"
+        getPopupContainer={() => navigatorBtnRef.current as HTMLDivElement}
+      >
+        <div className="navigator-btn" ref={navigatorBtnRef}>
+          <CgMenuGridO />
+        </div>
+      </Dropdown>
 
-			<NavbarList />
+      <NavbarList />
 
-			<div className="logo">
-				<span>Powered by</span>
-				<strong>IT IS IT</strong>
-			</div>
-		</aside>
-	);
+      <div className="logo">
+        <span>Powered by</span>
+        <strong>IT IS IT</strong>
+      </div>
+    </aside>
+  );
 }
