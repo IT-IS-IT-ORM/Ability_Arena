9c474c6927bb929939ef7fe92a706fbd12beb83d
YernarT <toktaryernar@gmail.com>
2024-01-10 22:06:00 +0600

    update: :hammer: 更精确的转换

--- client/src/utils/convertKeysCase.ts
+++ client/src/utils/convertKeysCase.ts
@@ -1,32 +1,43 @@
 import _ from "lodash";
 
-export function convertKeysCase<T=object>(
-  object: T,
+/**
+ * 检查变量是否是 JavaScript 中的基本类型
+ * 字符串、数字、布尔、null、undefined
+ * @param {any} value
+ * @returns {boolean}
+ */
+function isPrimitive(value: any): boolean {
+  return (
+    (typeof value !== "object" && typeof value !== "function") || value === null
+  );
+}
+
+/**
+ * @param {object} object
+ * @param {'camel' | 'snake'} case_
+ * @returns { object }
+ */
+export function convertKeysCase(
+  object: object,
   case_: "camel" | "snake"
-): T {
+): object {
+  if (isPrimitive(object)) return object;
+
   if (Array.isArray(object)) {
-    return object.map((item) => convertKeysCase(item, case_)) as T;
+    return object.map((item: object) => convertKeysCase(item, case_));
   }
 
-  if (typeof object === 'string') {
-    return _[`${case_}Case`](object) as T;
-  }
+  const newObj = {};
 
-  if (!_.isNumber(object)) {
-    const newObj = {};
+  _.forEach(object, (value, key) => {
+    if (_.isObject(value)) {
+      // @ts-ignore
+      newObj[_[`${case_}Case`](key)] = convertKeysCase(value, case_);
+    } else {
+      // @ts-ignore
+      newObj[_[`${case_}Case`](key)] = value;
+    }
+  });
 
-    _.forEach(object as T[], (value, key) => {
-      if (_.isObject(value)) {
-        // @ts-ignore
-        newObj[_[`${case_}Case`](key)] = convertKeysCase(value, case_);
-      } else {
-        // @ts-ignore
-        newObj[_[`${case_}Case`](key)] = value;
-      }
-    });
-
-    return newObj as T;
-  }
-
-  return object;
+  return newObj;
 }
