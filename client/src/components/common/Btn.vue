14c35ac81b561aceb8742cb0b35ff99ce20a1210
YernarT <toktaryernar@gmail.com>
2024-01-11 22:37:14 +0600

    feat: :sparkles: Btn 组件

---
+++ client/src/components/common/Btn.vue
@@ -0,0 +1,54 @@
+<template>
+    <button class="btn" :class="{ 'btn--block': props.block }">
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+defineComponent({ name: 'Btn' });
+
+const props = defineProps<{
+    block: boolean
+}>();
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style/mixins.scss';
+
+.btn {
+    // css vars
+    --c-box-shadow: #000;
+
+    color: #000;
+    font-weight: 400;
+    font-size: 20px;
+    letter-spacing: 2px;
+    text-decoration: none;
+    text-transform: uppercase;
+
+    @include flex($justifyContent: center, $alignItems: center, $gap: 12px);
+
+    padding: 12px 16px;
+    border: none;
+    outline: none;
+    background: #fff;
+    box-shadow: 1px 1px 1px 1px var(--c-box-shadow),
+        2px 2px 0 0 var(--c-box-shadow), 3px 3px 0 0 var(--c-box-shadow),
+        4px 4px 0 0 var(--c-box-shadow), 5px 5px 0 0 var(--c-box-shadow);
+    box-sizing: border-box;
+
+    cursor: pointer;
+    user-select: none;
+    touch-action: manipulation;
+    transition: transform var(--transition), box-shadow var(--transition);
+
+    &:active {
+        box-shadow: 0 0 0 0;
+        transform: translate(5px, 5px);
+    }
+
+    &--block {
+        width: 100%;
+    }
+}
+</style>
