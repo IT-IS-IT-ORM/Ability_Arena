634633f6e784acb9fc31dc3b479576bd3b7687e1
YernarT <toktaryernar@gmail.com>
2024-01-14 18:59:58 +0600

    init: 头像资源

---
+++ client/src/constants/resource.ts
@@ -0,0 +1,22 @@
+export default {
+  // 玩家头像
+  avatar: [
+    "/avatar/avatar_1.jpg",
+    "/avatar/avatar_2.jpg",
+    "/avatar/avatar_3.jpg",
+    "/avatar/avatar_4.jpg",
+    "/avatar/avatar_5.jpg",
+    "/avatar/avatar_6.jpg",
+    "/avatar/avatar_7.jpg",
+    "/avatar/avatar_8.jpg",
+    "/avatar/avatar_9.jpg",
+    "/avatar/avatar_10.jpg",
+    "/avatar/avatar_11.jpg",
+    "/avatar/avatar_12.jpg",
+    "/avatar/avatar_13.jpg",
+    "/avatar/avatar_14.jpg",
+    "/avatar/avatar_15.jpg",
+    "/avatar/avatar_16.jpg",
+    "/avatar/avatar_17.jpg",
+  ],
+};
