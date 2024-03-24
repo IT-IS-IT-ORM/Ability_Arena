e5c64f161a6721f52ec95b93050ee55832fcf98c
YernarT <toktaryernar@gmail.com>
2024-03-24 20:00:14 +0500

    init: :rainbow: routes config

---
+++ client/src/constants/routes.ts
@@ -0,0 +1,47 @@
+export default [
+  {
+    path: "/",
+    name: "main",
+    component: () => import("@/views/Main.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/Login.vue"),
+  },
+  {
+    path: "/settings",
+    name: "settings",
+    component: () => import("@/views/Settings.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/hero",
+    name: "hero",
+    component: () => import("@/views/Hero.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/thing",
+    name: "thing",
+    component: () => import("@/views/Thing.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/wiki",
+    name: "wiki",
+    component: () => import("@/views/Wiki.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+];
