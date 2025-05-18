86adc45c37f233ae702f98d9da77ff6b60ddb132
YernarT <toktaryernar@gmail.com>
2025-05-18 22:51:09 +0500

    feat: :sparkles: Auth API

--- server/src/apps/auth/main.js
+++ server/src/apps/auth/main.js
@@ -1,122 +1,25 @@
-import { AppDataSource } from "#root/typeorm.config.js";
-// Entity
-import { UserEntity } from "#root/apps/user/entity.js";
-// Serializer
-import { UserSerializer } from "#root/apps/user/serializer.js";
-// Redis
-import redis from "#root/redis/index.js";
-// Utils
-import { randomString } from "#root/utils/index.js";
-// Email
-import { sendMail } from "#root/email/index.js";
-import path from "path";
-import fs from "fs/promises";
-import ejs from "ejs";
+import User from "#src/db/collections/user.js";
+import { UserSerializer } from "#apps/user/serializer.js";
 
 export class AuthApi {
   constructor() {
-    this.userRepository = AppDataSource.getRepository(UserEntity);
+    this.userModel = User;
   }
 
-  getVerifyCode = async (ctx) => {
-    const { email, action } = ctx.request.body;
+  create = async (ctx) => {
+    const { username } = ctx.request.body;
 
-    const user = await this.userRepository.findOne({ where: { email } });
-
-    if (action === "register" && user) {
-      ctx.body = {
-        message: "auth.email_already_registered",
-      };
-      ctx.status = 400;
-      return;
-    }
-
-    if (action === "retrieve password" && !user) {
-      ctx.body = {
-        message: "auth.email_not_registered_yet",
-      };
-      ctx.status = 400;
-      return;
-    }
-
-    const code = await redis.get(`verify_code_${email}`);
-
-    if (!code) {
-      const templatePath = path.join(
-        process.cwd(),
-        "email/templates/verify_code_email.ejs"
-      );
-      const template = await fs.readFile(templatePath, "utf-8");
-      const verifyCode = randomString();
-      const html = ejs.render(template, {
-        code: verifyCode,
-        username: user?.username,
-      });
-
-      redis.set(`verify_code_${email}`, verifyCode, "EX", 60 * 5);
-
-      sendMail({
-        subject: "验证码 · Jana Dos",
-        html,
-        to: email,
-      });
-    }
+    const user = await this.userModel.create({ username });
 
     ctx.body = {
-      message: "auth.verify_code_sending_success",
+      data: await new UserSerializer(user).toJSON(),
     };
-    ctx.status = 200;
-  };
-
-  register = async (ctx) => {
-    const { username, password, email, verifyCode } = ctx.request.body;
-
-    const user = await this.userRepository.findOne({ where: { email } });
-
-    if (user) {
-      ctx.body = {
-        message: "auth.email_already_registered",
-      };
-      ctx.status = 400;
-      return;
-    }
-
-    const code = await redis.get(`verify_code_${email}`);
-
-    if (!code) {
-      ctx.body = {
-        message: "auth.verify_code_is_expired",
-      };
-      ctx.status = 400;
-      return;
-    }
-
-    if (verifyCode !== code) {
-      ctx.body = {
-        message: "auth.verify_code_is_incorrect",
-      };
-      ctx.status = 400;
-      return;
-    }
-
-    const newUser = this.userRepository.create({
-      username,
-      password,
-      email,
-    });
-
-    await this.userRepository.save(newUser);
-
-    ctx.body = {
-      data: await new UserSerializer(newUser, ctx).toJSON(),
-    };
-    ctx.status = 201;
   };
 
   login = async (ctx) => {
-    const { email, password } = ctx.request.body;
+    const { username } = ctx.request.body;
 
-    const user = await this.userRepository.findOne({ where: { email } });
+    const user = await this.userModel.findOne({ username });
 
     if (!user) {
       ctx.body = {
@@ -126,26 +29,17 @@ export class AuthApi {
       return;
     }
 
-    if (password !== user.password) {
+    if (user.isOnline) {
       ctx.body = {
-        message: "auth.username_or_password_is_incorrect",
+        message: "auth.account_already_online",
       };
       ctx.status = 400;
       return;
     }
 
     ctx.body = {
-      data: await new UserSerializer(user, ctx).toJSON(),
+      data: await new UserSerializer(user).toJSON(),
     };
     ctx.status = 200;
-  };
-
-  resetPassword = async (ctx) => {
-    const { email, verifyCode, password } = ctx.request.body;
-
-    ctx.body = {
-      message: "API 尚未实现",
-    };
-    ctx.status = 501;
   };
 }
--- server/src/apps/auth/routes.js
+++ server/src/apps/auth/routes.js
@@ -1,9 +1,7 @@
-import { apiRouter } from "#root/router/index.js";
+import { apiRouter } from "#src/router/index.js";
 import { AuthApi } from "#apps/auth/main.js";
 
 const authApi = new AuthApi();
 
-apiRouter.post("/auth/verify-code", authApi.getVerifyCode);
-apiRouter.post("/auth/register", authApi.register);
+apiRouter.post("/auth/create", authApi.create);
 apiRouter.post("/auth/login", authApi.login);
-apiRouter.post("/auth/reset-password", authApi.resetPassword);
