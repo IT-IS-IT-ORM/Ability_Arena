df88b3c416b60be37833fe23ce934f4c65ad1e36
YernarT <toktaryernar@gmail.com>
2025-05-19 18:47:32 +0500

    update: :hammer: use Layout

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <RouterView />
+  <Layout />
 </template>
 
 <script setup lang="ts">
 // Vue
-import { defineComponent, ref } from "vue";
-// Utilss
-import { isLandscape } from "@/utils";
+import { ref, onBeforeMount } from "vue";
+// i18n
+import i18nInstance from "@/i18n/index";
+// Utils
+import { isLandscape } from "@/utils/screen";
+import { localStorage } from "@/utils/localStorage";
+// Components
+import Layout from "@/components/Layout/index.vue";
 
-defineComponent({ name: "App" });
+defineOptions({ name: "App" });
 
 // 资源加载完毕 && 授权完成
 const loadResourceIsDone = ref(false);
+
+onBeforeMount(() => {
+  const language = localStorage.get<"en" | "cn" | "kz", "en">("language", "en");
+  i18nInstance.global.locale.value = language;
+});
 </script>
