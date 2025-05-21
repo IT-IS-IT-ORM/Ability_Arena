06bca658c7948674c9a5cd92196a66b782476822
YernarT <toktaryernar@gmail.com>
2025-05-21 17:17:45 +0500

    feat: :sparkles: connect sokcet when load

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -6,22 +6,27 @@
 <script setup lang="ts">
 // Vue
 import { ref, onBeforeMount } from "vue";
+// Store
+import { useSocketStore } from "@/store/socket";
 // i18n
 import i18nInstance from "@/i18n/index";
 // Utils
 import { localStorage } from "@/utils/localStorage";
 // Components
-import Layout from "@/components/Layout/index.vue";
+import Layout from "@/components/ui/Layout/index.vue";
 import ScreenWarning from "@/components/screen/ScreenWarning.vue";
 
 defineOptions({ name: "App" });
 
 // 资源加载完毕 && 授权完成
+const socketStore = useSocketStore();
+
 const loadResourceIsDone = ref(false);
 
 onBeforeMount(() => {
   const language = localStorage.get<"en" | "cn" | "kz", "en">("language", "en");
   i18nInstance.global.locale.value = language;
+  socketStore.connect();
 });
 </script>
 
