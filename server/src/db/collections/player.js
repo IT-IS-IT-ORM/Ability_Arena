028cf51a6c700c50106c770cbb7880caa73c187c
YernarT <toktaryernar@gmail.com>
2025-05-24 20:56:47 +0500

    update: :hammer: add timestamps

--- server/src/db/collections/player.js
+++ server/src/db/collections/player.js
@@ -1,27 +1,32 @@
 import mongoose from "mongoose";
 
-const playerSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    maxLength: 24,
+const playerSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      maxLength: 24,
+    },
+    avatarIndex: {
+      type: Number,
+      default: 1,
+      min: 1,
+    },
+    gold: {
+      type: Number,
+      default: 100,
+      min: 0,
+      max: 100_000_000,
+    },
+    isOnline: {
+      type: Boolean,
+      default: false,
+    },
   },
-  avatarIndex: {
-    type: Number,
-    default: 1,
-    min: 1,
-  },
-  gold: {
-    type: Number,
-    default: 100,
-    min: 0,
-    max: 100_000_000,
-  },
-  isOnline: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Player", playerSchema, "player");
