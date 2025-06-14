39416eb9e9720983e2cea1fc75eedac0a0003155
YernarT <toktaryernar@gmail.com>
2025-06-15 00:21:40 +0500

    update: :hammer: logout functionality

--- client/src/renderer/store/player.ts
+++ client/src/renderer/store/player.ts
@@ -3,6 +3,8 @@ import type { I_BasePlayer } from "@/type-def/Player";
 
 // Pinia
 import { defineStore } from "pinia";
+// Store
+import { useSocketStore } from "@/store/socket";
 // Utils
 import { localStorage } from "@/utils/localStorage";
 // Assets
@@ -18,25 +20,24 @@ import {
   Avatar_9,
 } from "@/assets/image/avatar";
 
-const getDefaultPlayer: () => I_BasePlayer = () => {
-  const player = localStorage.get<I_BasePlayer, I_BasePlayer>("player", {
-    _id: "",
-    username: "",
-    avatarIndex: 1,
-    mmr: 0,
-    isOnline: false,
-    team: null,
-    // Client Only
-    inRoom: false,
-    inGame: false,
-  });
-
-  return player;
-};
+const getDefaultPlayer: () => I_BasePlayer = () => ({
+  _id: "",
+  username: "",
+  avatarIndex: 1,
+  mmr: 0,
+  team: null,
+  isOnline: false,
+  // Client Only
+  inRoom: false,
+  inGame: false,
+});
 
 export const usePlayerStore = defineStore("playerStore", {
   state: () => ({
-    me: getDefaultPlayer(),
+    me: localStorage.get<I_BasePlayer, I_BasePlayer>(
+      "player",
+      getDefaultPlayer()
+    ),
     avatarList: [
       Avatar_1,
       Avatar_2,
@@ -55,6 +56,13 @@ export const usePlayerStore = defineStore("playerStore", {
       const patchedPlayer = { ...this.me, ...player };
       this.me = patchedPlayer;
       localStorage.set("player", patchedPlayer);
+    },
+
+    logout() {
+      this.setMe(getDefaultPlayer());
+      const socketStore = useSocketStore();
+      socketStore.disconnect();
+      socketStore.connect();
     },
   },
 
