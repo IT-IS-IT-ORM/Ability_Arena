0d4d45ed99e0983a4cc59a19d00ee07c60c2d1be
YernarT <toktaryernar@gmail.com>
2025-07-19 23:40:12 +0500

    feat: :sparkles: 属性收益

--- server/src/core/Attribute.js
+++ server/src/core/Attribute.js
@@ -7,4 +7,24 @@ export default {
   intelligence: "intelligence",
   // 全才
   universal: "universal",
+
+  // 属性收益
+  strengthGain: {
+    health: 20,
+    healthRecover: 0.63,
+    magicResistance: 0.15,
+  },
+  agilityGain: {
+    armor: 0.33,
+    baseAttack: 0.33,
+  },
+  intelligenceGain: {
+    mana: 13,
+    manaRecover: 0.82,
+    skillEnhancement: 0.25,
+  },
+  universalGain: {
+    // 基于全属性的基础攻击力加成系数
+    baseAttackFromAllAttribute: 0.7,
+  },
 };
